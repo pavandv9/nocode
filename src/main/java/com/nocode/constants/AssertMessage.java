1b5441ff4d8fe7f411d5208bab7b09a2182fa799
pavandv9 <pavandv9@gmail.com>
2021-08-30 17:02:17 +0530

    added assertion message

--- src/main/java/com/nocode/constants/AssertMessage.java
+++ src/main/java/com/nocode/constants/AssertMessage.java
@@ -10,6 +10,8 @@ public final class AssertMessage {
 
 	public static final String STATUS_CODE_FAILED = "Status code not matching";
 	public static final String RESPONSE_BODY_FAILED = "Response body not matching";
+	public static final String RESPONSE_BODY_EMPTY_FAILED = "Response body not matching expected to be empty but found not empty";
+	public static final String RESPONSE_BODY_NOT_EMPTY_FAILED = "Response body not matching expected to be not empty but found empty";
 
 	private AssertMessage() {}
 }
